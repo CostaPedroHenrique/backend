b2206d5a9528da22366aef49b475aefe6c099923
CostaPedroHenrique <devpedrohenrique@gmail.com>
2020-04-29 14:37:46 -0300

    Adicionando dependencias

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ const routes = require('express').Router();
 const multer = require('multer');
 const multerConfig = require('./config/multer');
 
-routes.post("/produtos", multer(multerConfig).single('file'), (req, res) => {
+routes.post("/produtos", multer(multerConfig).single('file'), (req, res) =>{
     console.log(req.file);
     return res.json({rotas:'funcionando!!!'});
 });
