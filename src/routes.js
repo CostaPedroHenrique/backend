df96745722843d9fcdff6ee5eeeffe5671a48443
CostaPedroHenrique <costa.henrique@academico.ifrn.edu.br>
2020-04-30 01:15:49 -0300

    removendo node_modules

--- src/routes.js
+++ src/routes.js
@@ -8,11 +8,11 @@ const knex = require('./database/index');
 routes.get('/colaboradores', ()=>
     knex('colaboradores').then((results) => results.json(results)))
 
-/*
+
 routes.post("/produtos", multer(multerConfig).single('file'), (req, res) =>{
     console.log(req.file);
     return res.json({rotas:'funcionando!!!'});
 });
-*/
+
 
 module.exports = routes;
